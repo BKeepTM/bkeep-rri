71c37e7b8f95258ca8e2750916076d825a06c446
JernejL123 <jernej.lobnik@gmail.com>
2026-01-19 21:16:42 +0100

    BR-10 dodal igro v loading screen

--- core/src/main/java/si/um/feri/projketRRI/utils/Constants.java
+++ core/src/main/java/si/um/feri/projketRRI/utils/Constants.java
@@ -5,13 +5,13 @@ import static si.um.feri.projketRRI.utils.MapRasterTiles.TILE_SIZE;
 import com.badlogic.gdx.Gdx;
 
 public class Constants {
-    public static final int NUM_TILES =5;//5
-    public static int ZOOM = 9;//9
+    public static final int NUM_TILES =5;//5 9
+    public static int ZOOM = 9;//9 10
     public static final int MAP_WIDTH = TILE_SIZE * NUM_TILES;
     public static final int MAP_HEIGHT = TILE_SIZE * NUM_TILES;
     public static final int HUD_WIDTH = Gdx.graphics.getWidth();
     public static final int HUD_HEIGHT = Gdx.graphics.getHeight();
-    public static final String API_URL = "https://pi.darkosever.si";
-    //public static final String API_URL = "http://localhost:3000";
+    //public static final String API_URL = "https://pi.darkosever.si";
+    public static final String API_URL = "http://localhost:3000";
 
 }
